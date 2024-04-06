988fc95a6658502059c9943f4b9165feeb247634
Gustave TSOPMO <gustaveckt@gmail.com>
2024-04-06 16:08:34 +0100

    Remove productseeder for production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call(CategorySeeder::class);
-        $this->call(ProductSeeder::class);
+        if(app()->isLocal()){
+            $this->call(ProductSeeder::class);
+        }
     }
 }
